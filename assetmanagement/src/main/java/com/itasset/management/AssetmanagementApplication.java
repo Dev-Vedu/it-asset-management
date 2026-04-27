ed6511d8959875b6d4568ae1bb34a27674edc8a0
Arya1315 <aryaingole535@email.com>
2026-04-27 21:48:48 +0530

    changes done

--- assetmanagement/src/main/java/com/itasset/management/AssetmanagementApplication.java
+++ assetmanagement/src/main/java/com/itasset/management/AssetmanagementApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AssetmanagementApplication {
+public class 	AssetmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AssetmanagementApplication.class, args);
